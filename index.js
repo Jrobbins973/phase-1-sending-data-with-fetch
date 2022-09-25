f10ca415d543df8d5a1a26d1aca504f6c821beb3
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-09-25 11:24:38 -0400

    commit tree a26705b4aa575d28006d4392be7da4c7294c493e

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Add your code here
+
+// const configurationObject = {
+//     method: 'POST',
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify()
+// }
+
+
+
+
+
+function submitData(uName, uEmail) {
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: uName,
+            email: uEmail,
+        }),    
+    }
+
+    let div = document.createElement("div")
+
+    return fetch(`http://localhost:3000/users`,configurationObject )
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        div.append(object.id)
+        document.body.appendChild(div)
+    }).catch(function (error) {
+        div.append(error.message)
+        document.body.appendChild(div)
+
+    });
+
+
+}
+
+// try {
+//     submitData()
+
+// } catch(error) {
+//     console.error(error)
+//     console.log('ITS ME THE ERROR')
+// }
+
+
+
+
+
